64d28dc1bb7e4e8939fad452fc2f62755c69fe5e
AsthaKirar <asthap602@gmail.com>
2024-10-15 00:44:54 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Nav from "./components/Nav"
 import Home from "./components/Home"
 import About from "./components/About"
 import Service from "./components/Service"
+import Pagenotfound from './components/Pagenotfound'
 
 const App = () => {
   return (
-    <div className='w-80% rounded-md my-10 bg-rose-400'>
+    <div className='w-[80%] rounded-md my-10 mt-10 mx-20  bg-rose-400'>
       <Nav/>
       <Routes>
         <Route path = "/" element={<Home/>}/>
-        <Route path = "/about" element={<About/>}/>
+        <Route path = "/About" element={<About/>}/>
         <Route path = "/service" element={<Service/>}/>
+        <Route path='*' element={<Pagenotfound/>} />
 
 
       </Routes>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,10 @@
-
+import { Link } from "react-router-dom"
 
 const Nav = () => {
   return (
     <div>
         <Link to ="/">Home</Link>
-        <Link to ="/about">About</Link>
+        <Link to ="/About">About</Link>
         <Link to ="/service">Service</Link>
     </div>
   )
